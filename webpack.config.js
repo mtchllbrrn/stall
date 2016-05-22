29fb7ec0f3d787069f4ab0518e10d3768c14d846
Mitchell Barron <hello@mitchellbarron.io>
2016-05-22 15:14:52 -0500

    Update webpack config for use with ES6 & React.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
-var webpack = require('webpack');
-var path = require('path');
+// const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
-var APP_DIR = path.resolve(__dirname, 'src/client/');
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const APP_DIR = path.resolve(__dirname, 'src/client/');
 
-var config = {
-  entry: APP_DIR + '/index.html',
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      include: APP_DIR,
+      loader: 'babel',
+    }],
+  },
 };
 
 module.exports = config;
